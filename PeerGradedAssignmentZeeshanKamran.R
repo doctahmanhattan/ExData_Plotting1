57354f5de38b6de37adceddd831341991ebe4e14
@doctahmanhattan <20890542+doctahmanhattan@users.noreply.github.com>
2024-06-27 22:49:02 -0400

    Update PeerGradedAssignmentZeeshanKamran.R

--- PeerGradedAssignmentZeeshanKamran.R
+++ PeerGradedAssignmentZeeshanKamran.R
@@ -1,17 +1,27 @@
+# Coursera Data Science: Exploratory Data Analysis
+# Course project 1
+# Zeeshan Kamran
 
-  
-  
-> file.url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-> dir.create("/Users/zeej12/Documents/Exploratory Data Analysis")
-> setwd("/Users/zeej12/Documents/Exploratory Data Analysis")
-> download.file(file.url,destfile = "/Users/zeej12/Documents/Exploratory Data Analysis/Dataset")
-> unzip("Dataset", exdir = "Exploratory Data Analysis", overwrite = TRUE)
-> xclass <- c(rep("character", 2), rep("numeric", 7)) 
-> consumptionofpower <- read.table("Exploratory Data Analysis/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = xclass)
-> columns <- c("Date", "Time", "GlobalActivePower", "GlobalReactivePower", "Voltage", "GlobalIntensity", "SubMetering1", "SubMetering2", "SubMetering3")
-> columns <- colnames(consumptionofpower)
-> install.packages("lubridate")
-> require("lubridate")  
-> consumptionofpower$DateTime <-dmy(consumptionofpower$Date) +hms(consumptionofpower$Time)
-> consumptionofpower <- consumptionofpower[, c(10, 3:9)]
-> write.table(consumptionofpower, file = "Exploratory Data Analysis/power_consumption.txt", sep = "|", row.names = FALSE)  
+## load the data
+rm(list = ls())
+data <- read.table("household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+# Plot 1
+attach(data)
+hist(Global_active_power, main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", col = "Red")
+
+# Save file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
+detach(data)
